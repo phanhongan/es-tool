2b13f276fef9e823faa69248998407b3dc9b76ca
Phan Hong An <phanhongan@gmail.com>
2016-11-15 19:09:29 +0700

    encode utf-8

--- es-tool.py
+++ es-tool.py
@@ -4,6 +4,8 @@ from elasticsearch import Elasticsearch, helpers, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import argparse
 import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 
 def parse_args():
